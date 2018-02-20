15e7210bc605a7356ca9b49b33f9508b23ef8932
Dylan Espinosa <espinosadylan17@gmail.com>
2018-02-19 16:21:43 -0800

    Ready for deploy

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says test is working!', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Test is passing!');
+        done();
+      });
+    });
 })
